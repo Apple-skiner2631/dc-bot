0f0d10b550e412437055bbbbc835ee072e05bcd0
Apple-skiner2631 <appleskiner2631@gmail.com>
2026-05-17 23:09:10 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -752,7 +752,7 @@ async def play_live(ctx, *, url: str):
         'quiet': True,
         'noplaylist': True,
         'skip_download': True,
-        'cookiefile': 'youtube_cookies.txt',
+        'cookiefile': 'youtube.com_cookies.txt',
         'http_headers': {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
         }
